fbaf84fda339e33ebcfd89b7423ce2a89669daff
viet.duong <viet.duong@asnet.com.vn>
2024-09-23 10:35:32 +0700

    Create folder Lazy

--- examples-react/src/main.tsx
+++ examples-react/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MarkdownEditor from './ExamLazy.tsx'
+import MarkdownEditor from './Lazy/ExamLazy.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
