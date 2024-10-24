b53c1f542e67630b125e35643347e46ea630a2fa
viet.duong <viet.duong@asnet.com.vn>
2024-10-24 17:51:09 +0700

    Create modal add edit delete UI

--- hotel-management/src/components/modal/rateModal/edit/index.tsx
+++ hotel-management/src/components/modal/rateModal/edit/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Modal,
   ModalOverlay,
