01f24665d88713e7f95a1e4709b82dbe54c77bb3
Viet Duong <viet.duong@asnet.com.vn>
2024-11-04 08:44:15 +0700

    Config root folder for importing

--- hotel-management/front-end/vite.config.ts
+++ hotel-management/front-end/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import EnvironmentPlugin from 'vite-plugin-environment'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin('all', { prefix: 'VITE_' })],
+  // root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./src")
+    },
+  },
 })
