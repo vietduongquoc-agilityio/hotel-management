fd6de07925f440f7846755d02b27f10d8a966acf
viet.duong <viet.duong@asnet.com.vn>
2024-11-15 17:37:36 +0700

    Remove header and update styles for main layout

--- hotel-management/front-end/src/layouts/Main/index.tsx
+++ hotel-management/front-end/src/layouts/Main/index.tsx
@@ -7,7 +7,7 @@ const MainLayout = () => {
   return (
     <Flex p="32px 15px" m="0 auto" w="1280px">
       <Sidebar />
-      <Box w="1020px" ml="20px">
+      <Box w="1020px" ml="20px" mt="40px">
         <Outlet />
       </Box>
     </Flex>
