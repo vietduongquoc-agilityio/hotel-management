5f83e7ce1306b7588d088bc40a788cebe1074808
viet.duong <viet.duong@asnet.com.vn>
2024-12-04 14:03:00 +0700

    Implement navitems file and update code in sidebar

---
+++ hotel-management/front-end/src/constants/navIteams.ts
@@ -0,0 +1,11 @@
+import roomIcon from "@/assets/icons/room.svg";
+import rateIcon from "@/assets/icons/rate.svg";
+import guestIcon from "@/assets/icons/guest.svg";
+import dealIcon from "@/assets/icons/deal.svg";
+
+export const navItems = [
+  { path: "/", label: "Room", icon: roomIcon },
+  { path: "/rate", label: "Rate", icon: rateIcon },
+  { path: "/guest", label: "Guest", icon: guestIcon },
+  { path: "/deal", label: "Deal", icon: dealIcon },
+];
